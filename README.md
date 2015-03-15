acf081e2f5d2e95fdac4ad818dc64c796947e2db
Haris Iqbal <harrisiqba@gmail.com>
2015-03-15 19:10:48 -0400

    Added Files, Email and Twilio Support

--- README.md
+++ README.md
@@ -8,6 +8,13 @@
 - Have a service to set off a buzzer /buzz
 - Have a service to stop the buzzer /stopbuzz
 - Have services to get weather conditions from the underground api onto the device /temperature/state(uppercase abbrev)/city, /humidity/state/city
+- Have a service to upload files /files/upload/sample.txt
+	use /upload.html to upload files to device
+- Have a service to view uploaded files to device in the files folder /files
+- Have a service to download file from the device /files/view/sample.txt
+- Have a service to delete files on the device in the files folder /files/delete/sample.txt
+- Have a service to text numbers /text/+18888888888
+- Have a service to email people /email/blah@gmail.com
 
 
 ## Requirements
@@ -15,5 +22,17 @@
 - Node and npm
 - Intel Edison
 - Seed Studio Grove sensors and actuators
+
+###Modules used
+
+- mraa
+- upm
+- express
+- twilio
+- busboy-connect
+- nodemailer
+- request
+- morgan
+- body-parser
 
 
--- server.js
+++ server.js
@@ -9,13 +9,17 @@ var morgan     = require('morgan');
 var mraa = require('mraa'); //require mraa
 var request = require('request');
 var LCD = require('jsupm_i2clcd');
+var busboy = require('connect-busboy');
+var fs = require('fs');
+var nodemailer = require('nodemailer');
+var twilio = require('twilio')('sid', 'key');
 
 app.use(morgan('dev')); // log requests to the console
 
 // configure body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(busboy());
 var port     = process.env.PORT || 8080; // set our port
 
 // ROUTES FOR OUR API
@@ -55,6 +59,14 @@ router.get('/stopbuzz', function(req, res) {
     stopBuzz()
     res.json({message: "stopped buzzing"})
 });
+router.get('/upload.html', function(req, res){
+    console.log("serving upload html")
+    res.sendfile(__dirname + '/static/upload.html')
+})
+router.get('/libs/jquery.min.js', function(req, res){
+    console.log("serving jquery")
+    res.sendfile(__dirname + '/static/libs/jquery.min.js')
+})
 router.get('/lcd/color/:color', function(req, res) {
     var color = req.params.color
     console.log('LCD color '+ color)
@@ -71,7 +83,7 @@ router.get('/lcd/print/:message', function(req, res) {
 });
 router.get('/temperature/:state/:city', function(req,res){
     var url = "http://api.wunderground.com/api"
-    var api_key = “ADDYOUROWN”
+    var api_key = "key"
     request({
         url: url + "/" + api_key + "/conditions/q/" + req.params.state + "/" + req.params.city +".json",
         json: true
@@ -87,7 +99,7 @@ router.get('/temperature/:state/:city', function(req,res){
 })
 router.get('/humidity/:state/:city', function(req,res){
     var url = "http://api.wunderground.com/api"
-    var api_key = "3f651b59a29eca3c"
+    var api_key = "key"
     request({
         url: url + "/" + api_key + "/conditions/q/" + req.params.state + "/" + req.params.city +".json",
         json: true
@@ -98,6 +110,84 @@ router.get('/humidity/:state/:city', function(req,res){
         }
         else{
             res.json({error:"Something Bad Happened"})
+        }
+    });
+})
+router.get('/files', function(req,res){
+    fs.readdir(__dirname + '/files', function(err, files){
+        if(err){
+            console.log(err)
+            res.json({"message": "could not read"})
+        }else{
+            res.json({"files":files})
+        }
+    })
+})
+router.get('/files/view/:name', function(req,res){
+    var filename = req.params.name
+    console.log('serving file name')
+    res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+    res.charset = 'UTF-8';
+    res.sendfile(__dirname + '/files/' + filename)
+})
+router.get('/files/delete/:name', function(req,res){
+    var filename = req.params.name
+    console.log('deleting file')
+    fs.unlink(__dirname + '/files/' + filename, function (err) {
+        if (err){
+            res.json({message: "delete failed"})
+            throw err;
+        }
+        else{
+            console.log('successfully deleted ' + __dirname + '/files/' + filename);
+            res.json({message: "deleted successfully"})
+        }
+    });
+})
+router.post('/upload/:filename',function(req, res) {
+    var fstream;
+    req.pipe(req.busboy);
+    var fileName = req.params.filename
+    req.busboy.on('file', function (fieldname, file, filename) {
+        console.log("Uploading: " + filename); 
+        fstream = fs.createWriteStream(__dirname + '/files/' + fileName);
+        file.pipe(fstream);
+        fstream.on('close', function () {
+            res.json({"completed":"true"});
+        });
+    });
+});
+router.get('/email/:address',function(req, res){
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'hiqbaldev@gmail.com',
+            pass: 'password'
+        }
+    });
+    var mailOptions = {
+        from: 'Haris Iqbal<hiqbaldev@gmail.com>', // sender address
+        to: req.params.address, // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: 'Hello world ✔', // plaintext body
+        html: '<b>Hello world ✔</b>' // html body
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            res.json({error: error})
+        }else{
+            res.json({recipient: req.params.address})
+        }
+    });
+})
+router.get("/text/:number", function(req,res){
+    twilio.sendMessage({
+        to: req.params.number, // Any number Twilio can deliver to
+        from: '+number', // A number you bought from Twilio and can use for outbound communication
+        body: 'Hey There :)' // body of the SMS message
+    }, function(err, responseData) { //this function is executed when a response is received from Twilio
+        if (!err) { // "err" is an error received during the request, if any
+            res.json({"to": req.params.number, "text":responseData.body})
         }
     });
 })
@@ -157,7 +247,6 @@ function lcdMessage(message){
 }
 function checkTouch() {
     var touchVal = touch.read(); //read the digital value of the pin
-    console.log(touchVal)
     if (touchVal) {
         lcdMessage("I am so touched");
     }
